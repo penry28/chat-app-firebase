1467fe4c37695059dcad542f39533cf6bc7bae67
penry28 <phamdinhhung28.it@gmail.com>
2022-05-17 12:23:58 +0700

    Done app

--- httpdocs/src/firebase/config.js
+++ httpdocs/src/firebase/config.js
@@ -9,13 +9,13 @@ import { getAuth, connectAuthEmulator } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBHhNOH-FoFQCZK2Bj_nUrdB8-cyq7vBKg",
-  authDomain: "chat-app-firebase-2c50b.firebaseapp.com",
-  projectId: "chat-app-firebase-2c50b",
-  storageBucket: "chat-app-firebase-2c50b.appspot.com",
-  messagingSenderId: "751058515323",
-  appId: "1:751058515323:web:e78dd2eba048212487f9de",
-  measurementId: "G-6C3L5654QM"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
@@ -24,8 +24,8 @@ const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-connectAuthEmulator(auth, "http://localhost:9099");
 if (['localhost', '127.0.0.1'].includes(window.location.hostname)) {
+  connectAuthEmulator(auth, "http://localhost:9099");
   connectFirestoreEmulator(db, 'localhost', 8081);
 }
 
